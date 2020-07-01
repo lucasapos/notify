23f023f6b40d16d9ddf0b6e78475e0441131b3ac
Lucas Oliveira <aplucasoliveira@gmail.com>
2020-07-01 00:04:43 -0300

    Teste fix

--- test/index.php
+++ test/index.php
@@ -3,7 +3,5 @@ require __DIR__ . "/../vendor/autoload.php";
 
 use Notification\Email;
 
-$novoEmail = new Email(2, "mail.rtlsolucoes.com.br", "contato@rtlsolucoes.com.br", "@rtlsolucoesjw20", "tls", "587", "contato@rtlsolucoes.com.br", "Equipe Nexo");
-$novoEmail->sendMail("Assunto de teste", "<h2>Lucas Oliveira</h2> <p>=)</p>", "lucas@nexodigital.com.br", "Oliveira Nexo", "aplucasoliveira@gmail.com", "Lucas Oliveira", );
-
-var_dump($novoEmail);
+$email = new Email(2, "mail.host.com", "your@email.com", "your-pass", "smtp secure (tls/ssl)", "port (587)", "from@email.com", "From Name");
+$email->sendMail("Subject", "Content", "reply@email.com", "Replay Name", "address@email.com", "Address Name");
